be780d67e589506597c911a2d87391848e6e40fd
Tom Cully <tom@blackraven.co.nz>
2020-03-28 20:57:38 +1300

    Redo init

--- client/clientjs/Remake.js
+++ client/clientjs/Remake.js
@@ -8,6 +8,10 @@ class Remake extends GameEngine {
 		this.debug = true
 		this.fullscreen = true
 
+		this.enableScroll = false
+		this.enableZoom = false
+		this.showHUD = false
+
 		this.addAsset('kobold.inner','assets/KoboldVillageInner.png')
 		this.addAsset('kobold.outer','assets/KoboldVillageOuter.png')
 
@@ -16,16 +20,18 @@ class Remake extends GameEngine {
 
 		this.addAudio('adventure','audio/130 even farther.mp3','audio/mpeg')
 
-		this.addMob('flik', 'flikWalk', Character, { offsetX: 128, offsetY: 128, currentAnimation: { name: 'walkeast', frame: 0, loop: true, delay: 120 } })
-		this.addMob('victor', 'victorWalk', Character, { offsetX: 156, offsetY: 128, currentAnimation: { name: 'walkeast', frame: 0, loop: true, delay: 120 } })
+		this.addMob('flik', 'flikWalk', Character, { offsetX: 128, offsetY: 128, tile: [1,3] })
+		this.addMob('victor', 'victorWalk', Character, { offsetX: 1024, offsetY: 128, tile: [1,2] })
 	}
 
 	mousedown() {
 		if(!this.playing) {
 			this.getAudio('adventure').play()
 			this.playing = true
-			this.getMob('flik').animate()
-			this.getMob('victor').animate()
+			setTimeout(()=>{
+				this.getMob('flik').animate({ name: 'walkeast', frame: 0, loop: true, delay: 120, dx: 10, maxX: 512, stopTile: [1,0] })
+				this.getMob('victor').animate({ name: 'walkwest', frame: 0, loop: true, delay: 120, dx: -10, minX: 600, stopTile: [1,0] })
+			},1000)
 		} else {
 			this.getAudio('adventure').stop()
 			this.playing = false
