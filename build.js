d3f451f799f0c941aeac8bf6d4f5fe9402f7c553
Tom Cully <tom@blackraven.co.nz>
2020-03-29 15:01:42 +1300

    Fix build double-compile issue

--- build.js
+++ build.js
@@ -10,21 +10,29 @@ const ncp = require('ncp')
 const async = require('async')
 const path = require('path')
 
-var logger = new Logger({logLevel: 'INFO'})
+var logger = new Logger({ logLevel: 'INFO' })
 
 logger.info("Building into directory 'build'...")
 
 async.series([
+	// Clear Old Build Dir
 	(cb) => { fs.rmdir('./build', { recursive: true }, cb) },
+	// Make a new one, and subfolders
 	(cb) => { fs.mkdir('./build', {}, cb) },
 	(cb) => { fs.mkdir('./build/css', {}, cb) },
 	(cb) => { fs.mkdir('./build/js', {}, cb) },
+	// Build the app
 	(cb) => {
 		async.parallel([
+			// Copy Index main
 			(cb) => { fs.copyFile('./client/views/index.hbs', './build/index.html', cb) },
+			// Copy All electron support files
 			(cb) => { ncp('./electron/', './build', cb) },
+			// Copy All assets etc
 			(cb) => { ncp('./client/public/', './build', cb) },
+			// Compile SASS
 			(cb) => { compileSASS('./build/css/app.css', cb) },
+			// Compile JS
 			(cb) => { compileJS('./build/js/app.js', cb) },
 		], cb)
 	}
@@ -38,8 +46,7 @@ async.series([
 
 function compileSASS(filename, cb) {
 	var sassEngine = new SassEngine({ logger: logger, recompile: false })
-	sassEngine.register('./client/sass/app.scss')
-	sassEngine.compile((err,src) => {
+	sassEngine.register('./client/sass/app.scss', (err,src) => {
 		if(err) return cb(err)
 		fs.writeFile(filename, src.css, cb)
 	})
@@ -48,8 +55,7 @@ function compileSASS(filename, cb) {
 
 function compileJS(filename, cb) {
 	var clientJSEngine = new ClientJSEngine({ logger: logger, beautify: false })
-	clientJSEngine.register(path.join(__dirname, './client', 'clientjs'))
-	clientJSEngine.compile((err, src) => {
+	clientJSEngine.register(path.join(__dirname, './client', 'clientjs'), (err, src) => {
 		if(err) return cb(err)
 		fs.writeFile(filename, src, cb)
 	})
