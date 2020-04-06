f325fa2de308e699334429aaf11dd5347228150f
Tom Cully <tom@blackraven.co.nz>
2020-04-06 17:22:39 +1200

    Mixin Doco

--- game/js/Mixin.js
+++ game/js/Mixin.js
@@ -1,3 +1,57 @@
+/*
+Mixins are a form of multiple inheritance for JavaScript.
+
+-- Writing Mixins -----------------
+To write a mixin class, do the following:
+
+```
+const Mixin = require('Mixin')
+
+// Define the class to extend Mixin
+class YourMixinClass extends Mixin {
+	static init(obj, options) {
+		// Do your initialisation here, like you would in a constructor.
+		// obj is the new object, eqivalent of 'this'.
+		// The user class can pass options, etc.
+		obj.testValue = 'This is a Mixin Class' 
+	}
+
+	// Add methods as per a standard class:
+	mixinMethod() {
+		...
+		console.log('mixinMethod: '+this.testValue)
+		...
+	}
+}
+
+// Use the Mixin.export() function to export the class.
+module.exports = Mixin.export(YourMixinClass)
+```
+
+-- Using Mixins -------------------
+To use your mixin class, do the following:
+
+```
+const YourMixinClass = require('YourMixinClass')
+
+class YourMixinUserClass {
+	constructor(options) {
+
+		// Call the mixin with this and any options
+		YourMixinClass(this, options)
+	}
+
+	anotherMethod() {
+		// Now you can use the properties and methods on the mixin
+
+		console.log(this.testValue)
+		this.mixinMethod()
+	}
+}
+
+```
+*/
+
 class Mixin {
 	static export(klass) {
 		return function(obj, options) {
@@ -25,7 +79,5 @@ class Mixin {
 	}
 }
 
-// See examples/MixinExample on how to write mixins
-// See examples/MixinExample on how to use mixins
-
 module.exports = Mixin
+
